0eff4d9f9eadfa0b6ad85a2eb58941f40c021335
geniusdo <2403644992@qq.com>
2025-06-12 18:54:49 +0800

    add cloud receiver

---
+++ msgs/cloud_msg/cloud_receiver.hpp
@@ -0,0 +1,31 @@
+// Copyright 2025 Chuangye Liu <chuangyeliu0206@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "../common.hpp"
+#include "cloud_msg.h"
+
+struct CloudReceiver {
+  using DataType                         = cloud_msg;
+  static constexpr uint8_t parser_type   = ParserType::Receiver;
+  static constexpr uint16_t header       = 0xD0FD;
+  static constexpr size_t length         = 1356;
+  static constexpr std::string_view name = "cloud_receiver";
+
+  static inline bool Process(const std::span<std::byte>& in, DataType& out) {
+    memcpy(out.data(), in.data(), length);
+    return true;
+  }
+};
